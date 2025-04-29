1fb1d42a1b0158497bc99011e99ceb2fec8f6907
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-04-29 12:15:43 -0700

    fixes

--- Sensor_Fusion.ino
+++ Sensor_Fusion.ino
@@ -9,17 +9,20 @@ void setup() {
 }
 
 void loop() {
+  ECE3_read_IR(sensorValues);
+
   const unsigned int maxValues[8] = {1627, 1696, 1249, 1201, 1081, 1252, 1587, 1721};
   const unsigned int minValues[8] = {873, 804, 711, 664, 646, 642, 711, 779};
-
-  // Read raw sensor values
-  ECE3_read_IR(sensorValues);
   double values[8];
 
-  // Print the sensor values as numbers from 0 to 2500
   for (unsigned char i = 0; i < 8; i++) {
-    values[i] = (sensorValues[i] - minValues[i]) * 1000.0 / maxValues[i];
-    Serial.print(values[i]);
+
+    int raw = sensorValues[i];
+    if (raw < minValues[i]) raw = minValues[i];
+    if (raw > maxValues[i]) raw = maxValues[i];
+
+    values[i] = (raw - minValues[i]) * 1000.0 / maxValues[i] - minValues[i];
+    Serial.print(sensorValues[i]);
     Serial.print('\t'); // Tab to format the raw data into columns in the Serial monitor
   }
 
@@ -29,5 +32,5 @@ void loop() {
   Serial.println(error);
 
   Serial.println();
-  delay(500);
+  delay(50);
 }
