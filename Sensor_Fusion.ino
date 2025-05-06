e9929a4f56e464cad8e76afe492c602075fe7525
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-06 09:51:43 -0700

    implement pd controller

--- Sensor_Fusion.ino
+++ Sensor_Fusion.ino
@@ -6,14 +6,16 @@ uint16_t sensorValues[8];
 const int left_nslp_pin = 31; // nslp ==> awake & ready for PWM
 const int left_dir_pin = 29;  // DIR_L
 const int left_pwm_pin = 40;  // PWML (pin #)
-                              //
+
 const int right_nslp_pin = 11;
 const int right_dir_pin = 30;
 const int right_pwm_pin = 39;
-// const int LED_RF = 41;
+
+// PD Controller variables
+double previous_error = 0.0;
+unsigned long previous_time = 0;
 
 void setup() {
-
   // for moving the car:
   pinMode(left_nslp_pin, OUTPUT);
   pinMode(left_dir_pin, OUTPUT);
@@ -23,18 +25,17 @@ void setup() {
   pinMode(right_dir_pin, OUTPUT);
   pinMode(right_pwm_pin, OUTPUT);
 
-  digitalWrite(right_dir_pin, LOW); // set this to high for donut!
-  digitalWrite(right_nslp_pin, LOW); // sleep? high = no
+  digitalWrite(right_dir_pin, LOW);   // set this to high for donut!
+  digitalWrite(right_nslp_pin, HIGH); // sleep? high = no
 
   digitalWrite(left_dir_pin, LOW);
-  digitalWrite(left_nslp_pin, LOW);// sleep? high = no
-
-  // pinMode(LED_RF, OUTPUT);
+  digitalWrite(left_nslp_pin, HIGH); // sleep? high = no
 
   ECE3_Init();
   Serial.begin(9600); // Set the data rate in bits per second for serial data
                       // transmission
   delay(2000);
+  previous_time = millis(); // Initialize previous_time
 }
 
 void loop() {
@@ -48,40 +49,66 @@ void loop() {
 
   // this goes for each sensor (8 sensors)
   for (unsigned char i = 0; i < 8; i++) {
-
     int raw = sensorValues[i];
     if (raw < minValues[i])
       raw = minValues[i];
     if (raw > maxValues[i])
       raw = maxValues[i];
 
-    values[i] = (double)(raw - minValues[i]) * 1000.0 / (maxValues[i] - minValues[i]);
+    // prevent zero division (not possible but just in case)
+    if (maxValues[i] == minValues[i]) {
+      values[i] = 0; // just set it to zero in this case to avoid /0
+    } else {
+      values[i] =
+          (double)(raw - minValues[i]) * 1000.0 / (maxValues[i] - minValues[i]);
+    }
     // Serial.print(sensorValues[i]);
-    // Serial.print( ' '); // Tab to format the raw data into columns in the Serial monitor
+    // Serial.print( ' '); // Tab to format the raw data into columns in the
+    // Serial monitor
   }
 
   // calculate the error based on all sensor values
   double error = (-8 * values[0] - 4 * values[1] - 2 * values[2] - values[3] +
                   values[4] + 2 * values[5] + 4 * values[6] + 8 * values[7]) /
-                 4.0;
+                 4.0; // we could trade 4.0 for the sum of the weight? idk
 
   // Serial.print("e:");
-  // Serial.println(error);
+  // Serial.print(error);
+
+  /////////////// PD CONTROLLER ///////////////////
+  unsigned long current_time = millis();
+
+  // Time difference in seconds
+  double dt_seconds = (double)(current_time - previous_time) / 1000.0;
+
+  // prevent division by zero (dt is too small)
+  if (dt_seconds == 0) {
+    dt_seconds = 1.0 / 50.0; // use a small dt instead of zero
+  }
+
+  double derivative = (error - previous_error) / dt_seconds;
+  // update for next iteration
+  previous_error = error;
+  previous_time = current_time;
 
   // this moves the car
-  int base_speed = 10;
-  float kp = 0.05;
+  int base_speed = 25;
+  float kp = 0.08;
+  float kd =
+      0.02; // how much should derivative affect this thing? needs to be tuned
 
-  float leftSpd_f = base_speed + (kp * error); // Use a float for calculation
-  float rightSpd_f = base_speed - (kp * error);
+  double adjustment = (kp * error) + (kd * derivative);
+
+  float rightSpd_f = base_speed + adjustment;
+  float leftSpd_f = base_speed - adjustment;
 
   int leftSpd_pwm = constrain(leftSpd_f, 0, 255); // Constrain to 0-255
   int rightSpd_pwm = constrain(rightSpd_f, 0, 255);
 
-  // Serial.print("l: ");
-  // Serial.println(leftSpd_pwm);
-
-  // Serial.print("r: ");
+  // Serial.print(", l: ");
+  // Serial.print(leftSpd_pwm);
+  //
+  // Serial.print(", r: ");
   // Serial.println(rightSpd_pwm);
 
   analogWrite(left_pwm_pin, leftSpd_pwm);
